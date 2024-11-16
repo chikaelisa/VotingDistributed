87fb13530e6e8c29a9fb85336eac87b30d655325
Luiz Henrique Firmino de Jesus <l176204@dac.unicamp.br>
2024-11-16 15:17:21 -0300

    feature: handle unexpected exceptions with SGE

--- VotingClient/src/MainClient.java
+++ VotingClient/src/MainClient.java
@@ -1,9 +1,17 @@
 import client.components.VotingClientFrame;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainClient {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(VotingClientFrame::new);
+        try {
+            SwingUtilities.invokeLater(VotingClientFrame::new);
+        } catch (HeadlessException headlessException) {
+            System.out.println("Operating System Graphic Environment (window system) not detected.");
+        } catch (Exception exceptionValue) {
+            System.out.println("An unexpected exception has been launched.");
+            System.out.println("Message: " + exceptionValue.getMessage() + "\n\n");
+        }
     }
 }
--- VotingServer/src/MainServer.java
+++ VotingServer/src/MainServer.java
@@ -1,9 +1,17 @@
 import server.components.VotingServer;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainServer {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(VotingServer::new);
+        try {
+            SwingUtilities.invokeLater(VotingServer::new);
+        } catch (HeadlessException headlessException) {
+            System.out.println("Operating System Graphic Environment (window system) not detected.");
+        } catch (Exception exceptionValue) {
+            System.out.println("An unexpected exception has been launched.");
+            System.out.println("Message: " + exceptionValue.getMessage() + "\n\n");
+        }
     }
 }
