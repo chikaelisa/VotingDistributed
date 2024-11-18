6ba2a952bea0fe72ac6086d14fe2843ed54fc47d
robkenha3 <robkenha@gmail.com>
2024-11-18 09:53:26 -0300

    Atualizado HelpServerMenuItem

--- VotingServer/src/server/components/menu/HelpServerMenuItem.java
+++ VotingServer/src/server/components/menu/HelpServerMenuItem.java
@@ -9,6 +9,15 @@ public class HelpServerMenuItem extends JMenuItem {
     }
 
     private void onHelpServerMenuItemSelected() {
-        // TODO: Implemente o menu de ajudar para o servidor
+        JOptionPane.showMessageDialog(null, 
+            "Bem-vindo ao sistema de votação eletrônica!\n\n" +
+            "Como usar o servidor:\n" +
+            "1. Inicie o servidor através do menu principal.\n" +
+            "2. Monitore os clientes conectados no painel de administração.\n" +
+            "3. Verifique os logs para acompanhar a atividade do sistema.\n" +
+            "4. Garanta que a configuração do firewall permita conexões seguras.\n" +
+            "Obs: Sempre faça backup regular dos dados e mantenha o servidor atualizado.\n\n" +
+            "Para mais informações, entre em contato com o suporte.",
+            "Ajuda", JOptionPane.INFORMATION_MESSAGE);
     }
 }
